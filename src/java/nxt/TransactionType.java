1578504c088c012c0b0f0078551076ddbd0dbd76
jonnytracker <jonnydjtracker@gmail.com>
2018-03-21 14:14:01 +0530

    transaction

--- src/java/nxt/TransactionType.java
+++ src/java/nxt/TransactionType.java
@@ -50,25 +50,16 @@ public abstract class TransactionType {
 
     private static final byte SUBTYPE_MESSAGING_ARBITRARY_MESSAGE = 0;
     private static final byte SUBTYPE_MESSAGING_ALIAS_ASSIGNMENT = 1;
-    private static final byte SUBTYPE_MESSAGING_POLL_CREATION = 2;
-    private static final byte SUBTYPE_MESSAGING_VOTE_CASTING = 3;
+    
+   
     private static final byte SUBTYPE_MESSAGING_HUB_ANNOUNCEMENT = 4;
     private static final byte SUBTYPE_MESSAGING_ACCOUNT_INFO = 5;
     private static final byte SUBTYPE_MESSAGING_ALIAS_SELL = 6;
     private static final byte SUBTYPE_MESSAGING_ALIAS_BUY = 7;
     private static final byte SUBTYPE_MESSAGING_ALIAS_DELETE = 8;
-    private static final byte SUBTYPE_MESSAGING_PHASING_VOTE_CASTING = 9;
+    
     private static final byte SUBTYPE_MESSAGING_ACCOUNT_PROPERTY = 10;
     private static final byte SUBTYPE_MESSAGING_ACCOUNT_PROPERTY_DELETE = 11;
-
-    private static final byte SUBTYPE_COLORED_COINS_ASSET_ISSUANCE = 0;
-    private static final byte SUBTYPE_COLORED_COINS_ASSET_TRANSFER = 1;
-    private static final byte SUBTYPE_COLORED_COINS_ASK_ORDER_PLACEMENT = 2;
-    private static final byte SUBTYPE_COLORED_COINS_BID_ORDER_PLACEMENT = 3;
-    private static final byte SUBTYPE_COLORED_COINS_ASK_ORDER_CANCELLATION = 4;
-    private static final byte SUBTYPE_COLORED_COINS_BID_ORDER_CANCELLATION = 5;
-    private static final byte SUBTYPE_COLORED_COINS_DIVIDEND_PAYMENT = 6;
-    private static final byte SUBTYPE_COLORED_COINS_ASSET_DELETE = 7;
 
     private static final byte SUBTYPE_DIGITAL_GOODS_LISTING = 0;
     private static final byte SUBTYPE_DIGITAL_GOODS_DELISTING = 1;
@@ -81,9 +72,6 @@ public abstract class TransactionType {
 
     private static final byte SUBTYPE_ACCOUNT_CONTROL_EFFECTIVE_BALANCE_LEASING = 0;
     private static final byte SUBTYPE_ACCOUNT_CONTROL_PHASING_ONLY = 1;
-
-    private static final byte SUBTYPE_DATA_TAGGED_DATA_UPLOAD = 0;
-    private static final byte SUBTYPE_DATA_TAGGED_DATA_EXTEND = 1;
 
     public static TransactionType findTransactionType(byte type, byte subtype) {
         switch (type) {
